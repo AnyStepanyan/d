16a5ecc83ed283063f2dd85c4556bcf1a0bfa733
Ani Stepanyan <118253116+AnyStepanyan@users.noreply.github.com>
2024-04-05 00:23:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 document.onkeydown = detectKey;
 let droneBox = document.getElementById('drone-box')
+let droneWrapper = document.getElementById('wrapper')
+
 function detectKey(e) {
-    let posLeft = document.getElementById('drone-box').offsetLeft;
-    let posTop = document.getElementById('drone-box').offsetTop;
-    if (e.keyCode == '38') {
-        droneBox.style.marginTop  = (posTop-50)+"px";
-    }
-    else if (e.keyCode == '40') {
-        droneBox.style.marginTop  = (posTop+50)+"px";
-    }
-    else if (e.keyCode == '37') {
-       droneBox.style.marginLeft  = (posLeft-50)+"px";
+    let posLeft = droneBox.offsetLeft;
+    let posTop = droneBox.offsetTop;
+    let bottom = droneBox.offsetParent.offsetHeight - posTop - droneBox.offsetHeight;
+    let top = droneBox.offsetParent.offsetHeight - bottom - droneBox.offsetHeight
+    let right = droneBox.offsetParent.offsetWidth  - posLeft - droneBox.offsetWidth - 50
+    let left = droneBox.offsetParent.offsetWidth  - right - droneBox.offsetWidth - 100
+
+
+    if (e.keyCode == '38'  ) {
+        if(top > 0 && top < 50){
+            droneBox.style.marginTop  = (posTop - top )+"px";
+        }else if(top >= 50) {
+            droneBox.style.marginTop  = (posTop-50)+"px";
+        }
+    }else if(e.keyCode == '40') {
+        if(bottom < 50 && bottom > 0){
+            droneBox.style.marginTop  = (posTop+bottom)+"px";
+        }else if(bottom >= 50){
+            droneBox.style.marginTop  = (posTop+50)+"px";
+        }    
+    }else if (e.keyCode == '37') {
+        if(left > 0 && left < 50 ) {
+            droneBox.style.marginLeft  = (posLeft-left)+"px";
+        }else if(left >=50){
+            droneBox.style.marginLeft  = (posLeft-50)+"px";
+        }
     }
     else if (e.keyCode == '39') {
-       droneBox.style.marginLeft  = (posLeft+50)+"px";
+        if(right > 0 && right < 50 ) {
+            droneBox.style.marginLeft  = (posLeft+right)+"px";
+        }else if(right >= 50){
+            droneBox.style.marginLeft  = (posLeft+50)+"px";
+        }
+
     }
+    console.log(right)
 }
